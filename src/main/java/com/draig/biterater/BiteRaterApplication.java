45fd8067c4bbffc7b30cd70a1c974cb3a3c5fe0c
grv <garv.sharma200212@gmail.com>
2025-09-02 23:18:39 +0530

    Initial commit

---
+++ src/main/java/com/draig/biterater/BiteRaterApplication.java
@@ -0,0 +1,13 @@
+package com.draig.biterater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteRaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiteRaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/draig/biterater/BiteRaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draig.biterater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteRaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
